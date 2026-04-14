c1c4695c02da6134fcb80b0ecd9e7ff51c6d2e2c
DariuxPy <dariuxpy@gmail.com>
2026-04-14 14:01:14 -0300

    Add files via upload

---
+++ holamundo.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Primera web con PHP</title>
+    <meta charset="UTF-8">
+</head>
+
+<body>
+    
+
+    <?php
+        $name="ALEJANDRA GONZALEZ";
+        $password="12345678";
+
+        echo "Usuario: ".$name;
+    ?>
+
+    <h1> hola mundo </h1>
+
+</body>
+</html>
